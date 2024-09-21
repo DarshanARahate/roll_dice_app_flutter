d7d7adbf82afb5fdb865be6204e5d52d635ccd5b
darshanrahate <rahate.darshan@gmail.com>
2024-09-21 17:45:50 +0530

    Initial Commit.

---
+++ lib/gradient_container.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'StyleText.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  final Color color1;
+  final Color color2;
+
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
+      ),
+    );
+  }
+}
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [
+//             Color.fromARGB(255, 26, 2, 80),
+//             Color.fromARGB(255, 45, 7, 98)
+//           ],
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyleText('Hello World'),
+//       ),
+//     );
+//   }
+// }
